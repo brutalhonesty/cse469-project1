b4a8b4a1eb9bd8c7a89d665899044c6e474a50f2
hellosputnik <hallosputnik@gmail.com>
2015-04-09 13:10:35 -0700

    Add comments to partition entry class
    I added the lecture notes as comments to the partition entry file.

--- partition_entry.py
+++ partition_entry.py
@@ -5,16 +5,17 @@ import binascii
 class PartitionEntry(object):
 
     def __init__(self, data):
-        self.boot_flag = struct.unpack('<c', data[:1])[0]
-        self.start_CHS_0 = struct.unpack('<B', data[1:2])[0]
-        self.start_CHS_1 = struct.unpack('<B', data[2:3])[0]
-        self.start_CHS_2 = struct.unpack('<B', data[3:4])[0]
-        self.partition_type = struct.unpack('<c', data[4:5])[0]
-        self.end_CHS_0 = struct.unpack('<B', data[5:6])[0]
-        self.end_CHS_1 = struct.unpack('<B', data[6:7])[0]
-        self.end_CHS_2 = struct.unpack('<B', data[7:8])[0]
-        self.start_lba = struct.unpack('<I', data[8:12])[0]
-        self.size_in_sectors = struct.unpack('<i', data[12:16])[0]
+
+        self.boot_flag = struct.unpack('<c', data[:1])[0]           # Current state of partition (0x00 = inactive, 0x80 active)
+        self.start_CHS_0 = struct.unpack('<B', data[1:2])[0]        # Beginning of partition (head)
+        self.start_CHS_1 = struct.unpack('<B', data[2:3])[0]        # Beginning of partition (cylinder)
+        self.start_CHS_2 = struct.unpack('<B', data[3:4])[0]        # Beginning of partition (sector)
+        self.partition_type = struct.unpack('<c', data[4:5])[0]     # Type of partition
+        self.end_CHS_0 = struct.unpack('<B', data[5:6])[0]          # End of partition (head)
+        self.end_CHS_1 = struct.unpack('<B', data[6:7])[0]          # End of partition (cylinder)
+        self.end_CHS_2 = struct.unpack('<B', data[7:8])[0]          # End of partition (sector)
+        self.start_lba = struct.unpack('<I', data[8:12])[0]         # Number of sectors between MBR and first sector in the partition
+        self.size_in_sectors = struct.unpack('<i', data[12:16])[0]  # Number of sectors in the partition
 
     def __str__(self):
         return 'Boot Flag: ' + str(self.to_value(self.boot_flag)) + '\n' \
